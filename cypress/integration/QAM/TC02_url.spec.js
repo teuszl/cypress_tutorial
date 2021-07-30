dd40162c705411da19178b68e7ed11625597c1d4
teuszl <teuszluczak@gmail.com>
2021-07-30 15:51:14 +0200

    pageObject

--- cypress/integration/QAM/TC02_url.spec.js
+++ cypress/integration/QAM/TC02_url.spec.js
@@ -1,15 +1,25 @@
+import testData from '../../fixtures/testData.json';
+import loginPage from '../../pages';
 describe('Create post, mark as favorite, then unfavorite', function(){
-    it('Sign in', function(){
+    this.beforeEach(() => {
+        loginPage.visit();
+        loginPage.fillEmail();
+        loginPage.fillPassword();
+        loginPage.logIn();
+
+    })
+
+    it('Sign in', () => {
         cy.visit('https://react-redux.realworld.io/#/login');
         cy.title().should('eq','Conduit');
         cy.location('protocol').should('eq','https:');
-        cy.get(':nth-child(1) > .form-control').type('teuszluczak@gmail.com').should('have.value', 'teuszluczak@gmail.com');
-        cy.get(':nth-child(2) > .form-control').type('tohaslo1!').should('have.value', 'tohaslo1!');
+        cy.get(':nth-child(1) > .form-control').type(testData.email).should('have.value', testData.email);
+        cy.get(':nth-child(2) > .form-control').type(testData.password).should('have.value', testData.password);
         cy.get('.btn').contains('Sign in').should('be.visible').click();
         cy.contains('Your Feed').should('be.visible');
     })
 
-    it('Create a post', function(){
+    it('Create a post', () =>{
         cy.contains('New Post').click();
         cy.hash().should('include', '#/editor');
         cy.get(':nth-child(1) > .form-control').type('myTitle').should('have.value', 'myTitle');
@@ -19,13 +29,13 @@ describe('Create post, mark as favorite, then unfavorite', function(){
         cy.get('.btn').contains('Publish Article').click();
     })
 
-    it('Verify if post is saved', function(){
+    it('Verify if post is saved', () =>{
         cy.hash().should('include', '#/article');
         cy.get('h1').should('have.text', 'myTitle');
         cy.get('p').should('have.text', 'myArticle');
     })
 
-    it('Mark as favorite', function(){
+    it('Mark as favorite', () =>{
         cy.get(':nth-child(4) > .nav-link').should('be.visible').click();
         cy.hash().should('include','#/@Teusz');
         cy.get(':nth-child(1) > .article-meta > .pull-xs-right > .btn').should('be.visible').click().should('have.css', 'background-color', 'rgb(92, 184, 92)');
--- cypress/pages/loginPage.js
+++ cypress/pages/loginPage.js
@@ -1,10 +1,21 @@
-import testData from '../../fixtures/testData.json'
-describe('Login', () =>{
+import testData from '../fixtures/testData.json'
 
-    it('Sign in', () =>{
+class LogInPage {
+    visit() {
         cy.visit('https://react-redux.realworld.io/#/login');
+        cy.title().should('eq','Conduit');
+        cy.location('protocol').should('eq','https:');
+    }
+    fillEmail() {
         cy.get(':nth-child(1) > .form-control').type(testData.email).should('have.value', testData.email);
+    }
+    fillPassword() {
         cy.get(':nth-child(2) > .form-control').type(testData.password).should('have.value', testData.password);
+    }
+    logIn() {
         cy.get('.btn').contains('Sign in').should('be.visible').click();
-    })
-})
+        cy.contains('Your Feed').should('be.visible');
+    }
+}
+
+export default new LogInPage();
